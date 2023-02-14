7f394fd17f2043affe26d2284eb34f51727e75c7
Nathanael Liechti <technat@technat.ch>
2023-02-14 20:25:35 +0100

    chore: add tf file to deploy function

---
+++ deploy/deploy.tf
@@ -0,0 +1,124 @@
+locals {
+  resource_prefix            = "js_weekend"
+  fcm_lambda_archive_version = "v0.0.2"
+  fcm_resource_name          = format("%s%s", local.resource_prefix, "form_confirmation_mail")
+  fcm_secret_data = {
+    "MAIL_HOST" = var.mail_host
+    "MAIL_FROM" = var.mail_from
+    "MAIL_USER" = var.mail_user
+    "MAIL_PW"   = var.mail_pw
+  }
+}
+
+resource "random_string" "random" {
+  length  = 8
+  special = false
+}
+
+output "fcm_webhook" {
+  value = aws_lambda_function_url.fcm_webhook.function_url
+}
+
+resource "aws_secretsmanager_secret" "fcm_secret" {
+  name                    = "${local.fcm_resource_name}-${random_string.random.result}"
+  recovery_window_in_days = 0
+
+  tags = local.common_tags
+}
+
+resource "aws_secretsmanager_secret_version" "fcm_secret_data" {
+  secret_id     = aws_secretsmanager_secret.fcm_secret.id
+  secret_string = jsonencode(local.fcm_secret_data)
+}
+
+resource "aws_iam_policy" "fcm_execution_policy" {
+  name        = local.fcm_resource_name
+  path        = "/"
+  description = "Policy for aws lamda ${local.fcm_resource_name}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        Effect   = "Allow"
+        Resource = "arn:aws:logs:${local.region}:${data.aws_caller_identity.current.account_id}:*"
+      },
+      {
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ]
+        Effect = "Allow"
+        Resource = [
+          aws_secretsmanager_secret.fcm_secret.arn
+        ]
+      }
+    ]
+  })
+
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role" "fcm_execution_role" {
+  name = local.fcm_resource_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "fcm_execution_binding" {
+  role       = aws_iam_role.fcm_execution_role.name
+  policy_arn = aws_iam_policy.fcm_execution_policy.arn
+}
+
+resource "null_resource" "fcm_lambda_code" {
+  triggers = {
+    always_run = timestamp() # this will always run
+  }
+
+  provisioner "local-exec" {
+    command = "wget https://github.com/the-technat/lambda_form_confirmation_mail/releases/download/${local.fcm_lambda_archive_version}/main.zip"
+  }
+}
+
+resource "aws_lambda_function" "fcm" {
+  function_name = local.fcm_resource_name
+  role          = aws_iam_role.fcm_execution_role.arn
+
+  filename = "${path.module}/main.zip"
+
+  runtime = "go1.x"
+  handler = "main"
+  timeout = "60"
+
+  environment {
+    variables = {
+      SECRET = "${local.fcm_resource_name}-${random_string.random.result}"
+    }
+  }
+
+  depends_on = [
+    null_resource.fcm_lambda_code
+  ]
+}
+
+resource "aws_lambda_function_url" "fcm_webhook" {
+  function_name      = aws_lambda_function.fcm.function_name
+  authorization_type = "NONE"
+}
